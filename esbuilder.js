caff955928047a49454efa1a961fb03436687c7d
KraXen72 <dpelech1@gmail.com>
2022-04-24 11:39:21 +0200

    refactor: esbuilder formatting

--- esbuilder.js
+++ esbuilder.js
@@ -1,14 +1,14 @@
 require('esbuild').buildSync({
-    entryPoints: ['src/main.ts', 'src/preload.ts', 'src/resourceswapper.ts', 'src/settingsui.ts', 'src/utils.ts'],
-    bundle: false,
-    format: 'cjs',
-    minify: false,
-    platform: 'node',
-    target: "es2020",
-    sourcemap: false,
-    banner: {
-      js: "\"use strict\";"
-    },
-    outdir: 'app',
-    tsconfig: 'tsconfig.json',
-  })
+	entryPoints: ['src/main.ts', 'src/preload.ts', 'src/resourceswapper.ts', 'src/settingsui.ts', 'src/utils.ts'],
+	bundle: false,
+	format: 'cjs',
+	minify: false,
+	platform: 'node',
+	target: "es2020",
+	sourcemap: false,
+	banner: {
+		js: "\"use strict\";"
+	},
+	outdir: 'app',
+	tsconfig: 'tsconfig.json',
+})
