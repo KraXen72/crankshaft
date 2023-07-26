ded9d33449a3dbe6dc3107a21847e6e69e6a64dd
KraXen72 <dpelech1@gmail.com>
2023-07-27 00:25:17 +0200

    chore: formatting

--- src/preload.ts
+++ src/preload.ts
@@ -208,7 +208,7 @@ export function getTimezoneByRegionKey(key: 'code' | 'id', value: string) {
 function patchSettings(_userPrefs: UserPrefs) {
 	// hooking & binding credit: https://github.com/asger-finding/anotherkrunkerclient/blob/main/src/preload/game-settings.ts
 	let interval: number | NodeJS.Timer = null;
-	strippedConsole.log('waiting to hook settings...')
+	strippedConsole.log('waiting to hook settings...');
 
 	function hookSettings() {
 		const settingsWindow = window.windows[0];
