e87bb39a6e087b9b0eb676217ef65f03968191b9
KraXen72 <dpelech1@gmail.com>
2023-08-14 15:50:49 +0200

    chore: add watch script to esbuilder

--- esbuilder.js
+++ esbuilder.js
@@ -1,10 +1,19 @@
 // crankshaft's build script. uses esbuild, which is a fast js build tool written in go.
+const esbuild = require('esbuild');
 
 const args = process.argv.filter(a => a.startsWith("--"))
 const building = args.includes("--build")
-console.log("building:", building)
+const watching = args.includes("--watch")
+console.log("building(minifying):", building, "watching:", watching)
 
-require('esbuild').buildSync({
+const buildLogger = {
+	name: 'build-logger',
+	setup(build) {
+		build.onEnd(result => console.log(`build completed with ${result.errors.length} errors`))
+	},
+}
+
+const buildOptions = {
 	// keep this manually in-sync!
 	entryPoints: [
 		'src/main.ts',
@@ -28,4 +37,16 @@ require('esbuild').buildSync({
 	},
 	outdir: 'app',
 	tsconfig: 'tsconfig.json',
-})
+	plugins: [ buildLogger ]
+}
+
+async function watch() {
+	const ctx = await esbuild.context(buildOptions)
+	await ctx.watch({  })
+}
+
+if (watching) {
+	watch();
+} else {
+	esbuild.buildSync(buildOptions)
+}
