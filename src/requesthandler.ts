6831dfd94f06f293b981a04da2a17a0f71f5e305
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-02-10 16:26:09 -0500

    Reimplement ad blocking to be compatible with resource swapper (#30)
    * reimplement ad blocking to be compatible with resource swapper
    
    * remove old comments
    
    * fix logic (oops)
    
    * lockfile
    
    * move back from adblock filters to url patterns
    
    * fix build

--- src/requesthandler.ts
+++ src/requesthandler.ts
@@ -1,52 +1,95 @@
 import { readdirSync, existsSync, mkdirSync } from 'fs';
 import { join as pathJoin } from 'path';
 
+import { Filter } from 'electron';
+import { readFileSync } from 'original-fs';
+
+// TODO: conditional import (?)
+import { URLPattern } from 'urlpattern-polyfill';
+
 const TARGET_GAME_DOMAIN = 'krunker.io';
 
 /// <reference path="global.d.ts" />
 
 /**
- * Swapper
- * @class Swapper
+ * RequestHandler
+ * @class RequestHandler
  */
 export default class {
 
 	private browserWindow: Electron.BrowserWindow;
 
-	private urls: string[] = [];
+	private swapperEnabled: boolean;
+
+	private blockerEnabled: boolean;
+
+	private customFiltersEnabled: boolean;
+
+	private filter: Filter = { urls: [] };
+
+	private swapUrls: string[] = [];
 
 	private started = false;
 
 	private swapDir: string;
+
+	private defaultFilters: string[];
+
+	private customFilters: string[] = [];
 
 	/**
 	 * Set the target window.
 	 *
 	 * @param browserWindow - The target window.
 	 */
-	public constructor(browserWindow: Electron.BrowserWindow, swapDir: string) {
+	// FIXME: better way to enable/disable?
+	public constructor(browserWindow: Electron.BrowserWindow, swapDir: string, swapperEnabled: boolean, blockerEnabled: boolean, customFiltersEnabled: boolean, defaultFiltersStr: string, customFiltersPath: string) {
 		this.browserWindow = browserWindow;
 		this.swapDir = swapDir;
+		this.swapperEnabled = swapperEnabled;
+		this.blockerEnabled = blockerEnabled;
+		this.customFiltersEnabled = customFiltersEnabled;
+
+		this.defaultFilters = defaultFiltersStr.split(/\r?\n/u);
+		this.customFilters = readFileSync(customFiltersPath, { encoding: 'utf-8' }).toString()
+			.split(/\r?\n/u)
+			.filter(filter => filter[0] !== '#');
 	}
 
-	/** Initialize the resource swapper for the target window.*/
+	/** Initialize the request handler for the target window.*/
 	public start(): void {
 		if (this.started) return;
 
 		// If the target directory doesn't exist, create it.
 		if (!existsSync(this.swapDir)) mkdirSync(this.swapDir, { recursive: true });
 
-		this.recursiveSwap('');
-
-		if (this.urls.length) {
-			this.browserWindow.webContents.session.webRequest.onBeforeRequest({ urls: this.urls }, (details, callback) => {
-				const path = new URL(details.url).pathname;
-				const resultPath = path.startsWith('/assets/') ? pathJoin(this.swapDir, path.substring(7)) : pathJoin(this.swapDir, path);
-
-				// Redirect to the local resource.
-				callback({ redirectURL: `krunker-resource-swapper:/${resultPath}` });
-			});
+		if (this.swapperEnabled) {
+			this.recursiveSwap('');
+			this.filter.urls.push(...this.swapUrls);
 		}
+
+		if (this.blockerEnabled) this.filter.urls.push(...this.defaultFilters);
+
+		if (this.customFiltersEnabled) this.filter.urls.push(...this.customFilters.filter(i => i !== ''));
+
+		this.browserWindow.webContents.session.webRequest.onBeforeRequest(this.filter, (details, callback) => {
+			if (this.swapperEnabled) {
+				const swapResourse = this.swapUrls.some(pat => new URLPattern(pat).test(details.url));
+				if (swapResourse) {
+					const path = new URL(details.url).pathname;
+					const resultPath = path.startsWith('/assets/') ? pathJoin(this.swapDir, path.substring(7)) : pathJoin(this.swapDir, path);
+
+					// Redirect to the local resource.
+					return callback({ redirectURL: `krunker-resource-swapper:/${resultPath}` });
+				}
+			}
+			if (this.blockerEnabled || this.customFiltersEnabled) {
+				const block = this.filter.urls.some(pat => new URLPattern(pat).test(details.url));
+				if (block) return callback({ cancel: true });
+			}
+			return callback({});
+		});
+
 
 		// Fix CORS problem with browserfps.com.
 		this.browserWindow.webContents.session.webRequest.onHeadersReceived(({ responseHeaders }, callback) => {
@@ -75,7 +118,7 @@ export default class {
 	}
 
 	/**
-	 * Recursively swap all files in the target directory.
+	 * Generate a list of url match patterns for all resources to swap (recursive)
 	 *
 	 * @param prefix - The target directory to swap.
 	 */
@@ -95,7 +138,7 @@ export default class {
 						`*://*.${TARGET_GAME_DOMAIN}/assets${name}`,
 						`*://*.${TARGET_GAME_DOMAIN}/assets${name}?*`
 					];
-					this.urls.push(...(/^\/(?:models|textures|sound|scares|videos)(?:$|\/)/u.test(name)
+					this.swapUrls.push(...(/^\/(?:models|textures|sound|scares|videos)(?:$|\/)/u.test(name)
 						? tests
 						: [
 							...tests,
